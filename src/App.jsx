93849383524a0c3d96ca2916daa6552275c54145
Oleksii Demchenko <demchenko.alexey.v@gmail.com>
2025-02-12 14:10:06 +0200

    Create basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import Greet from "./components/Greet.jsx";
+import {Header} from "./components/Header.jsx";
+import {Main} from "./components/Main.jsx";
+import Footer from "./components/Footer.jsx";
 
 const App = () => {
     return (
         <div>
-            <Greet />
+            <Header />
+            <Main />
+            <Footer />
         </div>
     )
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+export const Main = () => {
+    return (
+        <main>
+            <h2>Main Content</h2>
+            <p>This is main context paragraph.</p>
+        </main>
+    )
+}
+
+export default Main;
